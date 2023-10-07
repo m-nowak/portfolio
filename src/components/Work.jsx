6d27240fb2cd782a22a2cb816a043346e9d83485
Michal <cwiarinio@googlemail.com>
2023-10-07 14:18:29 +0100

    change text size

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -27,7 +27,7 @@ const Work = () => {
             >
               &nbsp;
             </motion.span>
-            <p className="mt-10 text-sm md:text-lg px-4 text-center">
+            <p className="mt-10 text-sm px-4 text-center">
               Please see sample projects. Keep in mind these are just simple
               examples.
             </p>
